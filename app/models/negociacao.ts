bdc3f1d1042963527ff02f952e96f53b6383695a
Rai Joia <megac154@gmail.com>
2023-11-24 16:48:41 -0300

    curso 1 finalizado

---
+++ app/models/negociacao.ts
@@ -0,0 +1,28 @@
+export class Negociacao {
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {
+    this._data = _data
+    this._quantidade = _quantidade
+    this._valor = _valor
+  }
+
+  get data(): Date {
+    const data = new Date(this._data.getTime())
+    return data
+  }
+
+  get quantidade(): number {
+    return this._quantidade
+  }
+
+  get valor(): number {
+    return this._valor
+  }
+
+  get volume(): number {
+    return this._quantidade * this._valor
+  }
+}
