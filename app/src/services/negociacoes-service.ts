9befca830c463e6a90d91999c0b9ab6eb2953b16
Rai Joia <megac154@gmail.com>
2023-11-28 15:19:36 -0300

    interface com services

---
+++ app/src/services/negociacoes-service.ts
@@ -0,0 +1,14 @@
+import { Negociacao } from "../models/negociacao.js"
+import { NegociacaoDoDia } from "../interfaces/negociacao-do-dia.js"
+
+export class NegociacoesService {
+  public obterNegociacoes(): Promise<Negociacao[]> {
+    return fetch("http://localhost:8080/dados")
+      .then(res => res.json())
+      .then((dados: NegociacaoDoDia[]) => {
+        return dados.map(dadoDeHoje => {
+          return new Negociacao(new Date(), dadoDeHoje.vezes, dadoDeHoje.montante)
+        })
+      })
+  }
+}
